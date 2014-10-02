671e2a557d077eae906800a3650cac70504b3e2b
Syed Muhammad Mahmudul Haque <mmhyamin@gmail.com>
2014-10-02 20:10:58 +0600

    Fix header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+(function($,sr) {
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+    var timeout;
+    return function debounced () {
+      var obj = this, args = arguments;
+      function delayed () {
+        if (!execAsap)
+                          func.apply(obj, args);
+        timeout = null;
+      }
+      ;
+      if (timeout)
+                    clearTimeout(timeout); else if (execAsap)
+                    func.apply(obj, args);
+      timeout = setTimeout(delayed, threshold || 100);
+    }
+    ;
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn) {
+    return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
+  }
+  ;
+}
+)(jQuery,'smartresize');
+
 $(document).ready(function(){
     new WOW().init();
 		$("#client-speech").owlCarousel
@@ -8,6 +36,29 @@ $(document).ready(function(){
 			paginationSpeed : 1000,
 			singleItem:true
 		});
+
+    var setHomeBannerHeight = function(){
+   var homeHeight= $(window).height();
+   $('#overlay-1').height(homeHeight);
+    }
+    setHomeBannerHeight();  
+
+     var $scrollDownArrow = $('.bottom > a');
+  var animateScrollDownArrow = function() {
+    $scrollDownArrow.animate( {
+      top: 5,
+    }
+    , 400, "linear", function() {
+      $scrollDownArrow.animate( {
+        top: -5,
+      }
+      , 400, "linear", function() {
+        animateScrollDownArrow();
+      }
+      );
+    });
+  }
+  animateScrollDownArrow();  
 	});
 
 var menu = $('#navigation');
@@ -28,3 +79,4 @@ function scroll()
        //$('.content').removeClass('menu-padding');
     }
 }
+
