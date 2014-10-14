febd4ef2bfe793ff22cfd0aefed698b95656c93d
Syed Muhammad Mahmudul Haque <mmhyamin@gmail.com>
2014-10-14 22:45:56 +0600

    RC-1 Push 10.14.14
    Simplifying Design and Code

--- js/script.js
+++ js/script.js
@@ -1,30 +1,82 @@
+///////////////////////////////
+// Smart Resize
+///////////////////////////////
+
 (function($,sr) {
-  // debouncing function from John Hann
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-  var debounce = function (func, threshold, execAsap) {
-    var timeout;
-    return function debounced () {
-      var obj = this, args = arguments;
-      function delayed () {
-        if (!execAsap)
-                          func.apply(obj, args);
-        timeout = null;
-      }
-      ;
-      if (timeout)
-                    clearTimeout(timeout); else if (execAsap)
+    var debounce = function (func, threshold, execAsap) {
+        var timeout;
+        return function debounced () {
+            var obj = this, args = arguments;
+            function delayed () {
+                if (!execAsap)
                     func.apply(obj, args);
-      timeout = setTimeout(delayed, threshold || 100);
+                    timeout = null;
+            };
+            if (timeout)
+                clearTimeout(timeout); else if (execAsap)
+                func.apply(obj, args);
+                timeout = setTimeout(delayed, threshold || 100);
+        };
     }
-    ;
-  }
-  // smartresize 
-  jQuery.fn[sr] = function(fn) {
-    return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
-  }
-  ;
+  
+    // smartresize 
+    jQuery.fn[sr] = function(fn) {
+        return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
+    };
+})
+
+(jQuery,'smartresize');
+
+
+$(function() {
+
+///////////////////////////////
+// Fix the Home Height
+///////////////////////////////
+
+    var setHomeBannerHeight = function(){
+        var homeHeight= $(window).height();
+        $('#overlay-1').height(homeHeight);
+    }
+
+    setHomeBannerHeight();
+
+///////////////////////////////
+// One page Smooth Scrolling
+///////////////////////////////
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            return false;
+        }
+    }
+});
+
+///////////////////////////////
+// Center Home Slideshow Text
+///////////////////////////////
+
+function centerHomeBannerText() {
+    var bannerText = jQuery('#wrapper .starting-text');
+    var bannerTextTop = (jQuery('#wrapper').actual('height')/2) - (jQuery('#wrapper .starting-text').actual('height')/2) - 20;
+    bannerText.css('padding-top', bannerTextTop+'px');
+    bannerText.show();
 }
-)(jQuery,'smartresize');
+
+centerHomeBannerText();
+
+jQuery(window).smartresize(function() {
+    setHomeBannerHeight();
+    centerHomeBannerText();
+});
+    
+});
 
 $(document).ready(function(){
     new WOW().init();
@@ -43,40 +95,27 @@ $(document).ready(function(){
     }
     setHomeBannerHeight();  
 
-     var $scrollDownArrow = $('.bottom > a');
-  var animateScrollDownArrow = function() {
-    $scrollDownArrow.animate( {
-      top: 5,
-    }
-    , 400, "linear", function() {
-      $scrollDownArrow.animate( {
-        top: -5,
-      }
-      , 400, "linear", function() {
-        animateScrollDownArrow();
-      }
-      );
-    });
-  }
-  animateScrollDownArrow();  
+       
 	});
 
-var menu = $('#navigation');
 
 
-function scroll() 
-{ var origOffsetY = menu.offset().top;
-   if ($(window).scrollTop() >= origOffsetY) 
-   {
-       $('#navigation').addClass('nav-wrap');
-       $('#about').addClass('exp');
-       //$('.content').addClass('menu-padding');
-   } 
-   else 
-   {
-       $('#navigation').removeClass('nav-wrap');
-       $('#about').removeClass('exp');
-       //$('.content').removeClass('menu-padding');
-    }
-}
+
+
+$(document).ready(function(){
+
+  var menu = $('#navigation > .navbar');
+  var origOffsetY = $('#bottom').offset().top;
+
+  function scroll() {
+     if ($(window).scrollTop() > origOffsetY) {
+        menu.addClass('navbar-white');
+     } else {
+        menu.removeClass('navbar-white');
+     }
+  }
+
+  document.onscroll = scroll;
+
+});
 
